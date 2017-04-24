8999ab7889f6f09f89d70bc977f50364a797afff
Alex Espinosa <espinosa_alex@yahoo.com>
2017-04-24 18:25:34 +0200

    finished basic boat controller

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -8,18 +8,30 @@ class BoatsController < ApplicationController
   end
 
   def create
-
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to dashboard_path
+    else
+      render 'new'
+    end
   end
 
   def index
+    @boats_by_city = Boat.find(params[:city])
   end
 
   def edit
+    @boat = Boat.find(params[:id])
   end
 
   def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
   end
 
-  def update
+  private
+
+  def boat_params
+    params.require(:boat).permit(:type, :city, :price, :address, :capacity, :user_id, :name)
   end
 end
