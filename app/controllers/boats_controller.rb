d456802e976038727ee7f706d4e73613aa64c5c5
Joe Schafer <joeschaferastula@gmail.com>
2017-04-24 17:23:38 +0200

    added controllers

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,22 @@
+class BoatsController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def edit
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
