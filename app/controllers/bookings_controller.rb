099f875b4394c6bacd8282979727cb3ccdbd7620
Alex Espinosa <espinosa_alex@yahoo.com>
2017-04-24 18:40:04 +0200

    finished booking controller basics

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,30 @@
 class BookingsController < ApplicationController
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to new_boat_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
-  def update
-  end
 
-  def show
+  private
+
+  def booking_params
+    params.require(:booking).permit(:boat_id, :user_id, :check_out, :check_in)
   end
 end
