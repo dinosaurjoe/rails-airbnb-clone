78eaab96571861a9bbefabbf58c94ab77bdebb42
Alex Espinosa <espinosa_alex@yahoo.com>
2017-04-25 16:46:36 +0200

    almost done with facebook login and added logged in navbar changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :boats
   resources :bookings, only: [:new, :create, :update, :edit, :show]
   resources :users, only: [:edit, :update, :show]
