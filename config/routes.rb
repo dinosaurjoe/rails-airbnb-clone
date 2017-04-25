6bdd71a92c83cbc71a7017fe2ef99a0f66952cf5
Alex Espinosa <espinosa_alex@yahoo.com>
2017-04-25 11:08:00 +0200

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :bookings, only: [:new, :create, :edit, :update, :show]
   resources :users, only: [:edit, :update, :show]
   get '/dashboard' => 'dashboards#show'
-  root to: 'home#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
