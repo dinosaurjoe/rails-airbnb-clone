81687e030ce51eebdf0ea74c7e9fa2ec9f7c9de6
Alex Espinosa <espinosa_alex@yahoo.com>
2017-04-25 10:24:37 +0200

    basic user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
