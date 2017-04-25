e57075f3b92a06db6ede18b4838a622db4ede10b
Alex Espinosa <espinosa_alex@yahoo.com>
2017-04-25 16:59:43 +0200

    still working on fb login, home page doesn't work

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :boats
   resources :bookings, only: [:new, :create, :update, :edit, :show]
   resources :users, only: [:edit, :update, :show]
   get 'dashboard' => 'dashboards#show'
-  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
