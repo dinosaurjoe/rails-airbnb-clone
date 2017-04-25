ad8ad5eeb07fb931a1730d4d2476dacf487de734
Alex Espinosa <espinosa_alex@yahoo.com>
2017-04-25 11:15:53 +0200

    Revert "Revert "created seeds, added user first_name and user_last_name""

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home :index :user_show :boat_show
+
   def home
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+password = Faker::Internet.password(8)
+
+20.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: password,
+    password_confirmation: password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Food.ingredient
+    )
+end
