de5c5730cf75bebe34d2413e13638983b4888abb
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-04-27 17:37:52 +0200

    added a helper for the restriction

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,6 @@ class DashboardsController < ApplicationController
   def show
     @user = current_user
     @boats = @user.boats
-    # @boat_requests = @boats.bookings
     @bookings = @user.bookings
   end
 end
