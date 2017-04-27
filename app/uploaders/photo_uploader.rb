26733668cf9b923e11a9153facc6a0d92154bd87
Joe Schafer <joeschaferastula@gmail.com>
2017-04-27 18:07:50 +0200

    added default image for boat

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,6 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path([version_name, "sailboat.jpg"].compact.join('_'))
+  end
 end
